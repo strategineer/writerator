9404da96879a5a95287c799e976fe0b90e149022
Bill Tyros <tyros.bill@gmail.com>
2012-12-03 19:57:36 -0500

    add Apache License 2.0

--- text_scan.py
+++ text_scan.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+# Copyright 2012 Bill Tyros
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import logging
 import sys
@@ -25,15 +39,15 @@ def main():
                        Either: w for words, l for letters""",
                        choices=['w', 'l'])
     
-    #Optional arguments
     
+    #Optional arguments
     parser.add_argument("-d", "--debug", help="displays logging debug messages",
                 action="store_true")
     
     parser.add_argument("-r", "--rank", type=int, metavar="NUMBER",
                    help="""counts the total occurences of each word and ranks
                    them""")
-        
+
     args = parser.parse_args()
     
     
