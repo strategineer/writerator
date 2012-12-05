56c057d8588174907c77ee614670d2f7c279dc81
Bill Tyros <tyros.bill@gmail.com>
2012-12-05 15:43:21 -0500

    reorder Text methods for clarity, add count methods

--- texttools.py
+++ texttools.py
@@ -160,24 +160,20 @@ class Text(BasicText):
     # characters(c), phrases(p)
     _types_of_elements = ['w', 'c', 'p']
     
-    def __init__(self, filename):
-        """Initializes a Text."""
-        super( Text, self ).__init__(Text.get_text_from_txt_file(filename))
-    
     @staticmethod
-    def _clean_input_elements(elements, kind):
+    def _clean_input_elements(elements, element_type):
         """
             Cleans a list of elements by removing unnecessary punctuation,
             whitespace from each element, removing the empty strings from the
             list and returns the result.
         """
-        if kind == Text._types_of_elements[0]:
+        if element_type == Text._types_of_elements[0]:
             elements = [x.strip("(),. ") for x in elements]
         
-        elif kind == Text._types_of_elements[1]:
+        elif element_type == Text._types_of_elements[1]:
             pass
         
-        elif kind == Text._types_of_elements[2]:
+        elif element_type == Text._types_of_elements[2]:
             elements = [x.strip() for x in elements]
         
         else:
@@ -203,85 +199,52 @@ class Text(BasicText):
             logging.error("No such filename cannot get text: " + filename_in)
             sys.exit(0)
     
-    def _parse_phrases(self):
-        """Parses a Text and returns a generator containing the phrases as Phrases"""
-        phrases = self.text.split(".")
-            
-        phrases = Text._clean_input_elements(phrases, 'p')
-        return [Phrase(phrase) for phrase in phrases]
-    
-    def _parse_words(self):
-        """Parses a Text and returns a generator containing the words as Words"""
-        words = self.text.split(" ")
-        
-        words = Text._clean_input_elements(words, 'w')
-        return [Word(word) for word in words]
-    
-    def _parse_characters(self):
-        """Parses a Text and returns a generator containing the characters as Characters"""
-        characters = list(self.text)
-        
-        characters = Text._clean_input_elements(characters, 'c')
-        return [Character(character) for character in characters]
-    
-    def _split_text_by_element(self, kind):
-        """
-            Splits the text by element and returns a list
-            
-            i.e. "Bill. Is. Cool." => Words (w) => ["Bill.", "Is.", "Cool."]
-            "Bill. Is. cool." => Characters (c) => ["B", "i", "l", "l", "I", "s", ...]
-            "Bill. Is very. cool." => Phrases (p) => ["Bill", "Is very", "Cool"]
-        """
-        if kind in Text._types_of_elements:
-            if kind == Text._types_of_elements[0]:
-                return self._parse_words()
-            
-            elif kind == Text._types_of_elements[2]:
-                return self._parse_phrases()
+    def __init__(self, filename):
+        """Initializes a Text."""
+        super( Text, self ).__init__(Text.get_text_from_txt_file(filename))
 
-            elif kind == Text._types_of_elements[1]:
-                return self._parse_characters()
-        
-        else:
-            logging.error("No such type available cannot split: " + kind)
-            logging.error("try: " + str(Text._types_of_elements))
-            sys.exit(0)
+
+    def count_words(self):
+        """Counts the number of words in a Text."""
+        return len(self._parse_words(cast_as_objects=False))
     
-    def make_occurences_Counter(self, kind):
+    def count_phrases(self):
+        """Counts the number of Phrases in a Text."""
+        return len(self._parse_phrases(cast_as_objects=False))
+    
+    def count_characters(self):
+        """Counts the number of characters in a Text."""
+        return len(self._parse_characters(cast_as_objects=False))
+    
+    def calculate_Gunning_Fog_Index(self):
+        """Calculates and returns the text's Gunning-Fog index."""
+        words = self._parse_words()
+        complex_words = [word for word in words if word.countSyllables() >= 3 and not word.istitle()]
+        
+        number_of_words = len(words)
+        number_of_complex_words = len(complex_words)
+        number_of_sentences = self.count_phrases()
+        
+        return (0.4) * ( (number_of_words / number_of_sentences) 
+                         + 100 * (number_of_complex_words / number_of_words) )
+
+
+    def make_occurences_Counter(self, element_type):
         """Returns a Counter with the elements decided by kind, either
          words, characters or phrases as keys from within a Text."""
-        elements = self._split_text_by_element(kind)    
+        elements = self._parse_elements(element_type)    
         return Counter( [str(x) for x in elements] )
         
-    def count_occurences(self, element_to_count, kind):
+    def count_occurences(self, element_to_count, element_type):
         """Return the total number of times an element appears in a Text."""
-        occurences = self.make_occurences_Counter(kind)
+        occurences = self.make_occurences_Counter(element_type)
             
         if element_to_count in occurences:
             return occurences[element_to_count]
         else:
             return 0
         
-    def rank_by_total_count(self, kind):
-        """
-            Returns a list of tuples containing the number of occurrences of each
-            element [words (w), characters (c) or phrases (p)] found in the Text and
-            the str form of the elements.
-            
-            The list is sorted by number of occurences in decreasing order.
-        """
-        if kind in Text._types_of_elements:
-            occurences = self.make_occurences_Counter(kind)
-            
-            return occurences.most_common()
-        
-        else:
-            logging.error("No such type available cannot rank by occurrences: "
-                          + kind)
-            logging.error("try: " + str(Text._types_of_elements))
-            sys.exit(0)
-    
-    def rank_by_number_of_matches(self, matches_to_check, kind):
+    def rank_by_number_of_matches(self, matches_to_check, element_type):
         """
             Returns a list of tuples containing the number of matches found in
             each element [words (w), characters (c) or phrases (p)] of the Text and
@@ -291,8 +254,8 @@ class Text(BasicText):
         """
         assert isinstance(matches_to_check, list)
         
-        if kind in Text._types_of_elements:
-            elements = self._split_text_by_element(kind)
+        if element_type in Text._types_of_elements:
+            elements = self._parse_elements(element_type)
             set_of_elements = set(elements)
             
             ranked_by_matches = []
@@ -310,22 +273,28 @@ class Text(BasicText):
             return ranked_by_matches
         
         else:
-            logging.error("No such type available cannot rank by matches: " + kind)
+            logging.error("No such type available cannot rank by matches: " + element_type)
             logging.error("try: " + str(Text._types_of_elements))
             sys.exit(0)
-    
-    def calculate_Gunning_Fog_Index(self):
-        """Calculates and returns the text's Gunning-Fog index."""
-        words = self._split_text_by_element(Text._types_of_elements[0])
-        complex_words = [word for word in words if word.countSyllables() >= 3 and not word.istitle()]
-        sentences = self._split_text_by_element(Text._types_of_elements[2])
+
+    def rank_by_total_count(self, element_type):
+        """
+            Returns a list of tuples containing the number of occurrences of each
+            element [words (w), characters (c) or phrases (p)] found in the Text and
+            the str form of the elements.
+            
+            The list is sorted by number of occurences in decreasing order.
+        """
+        if element_type in Text._types_of_elements:
+            occurences = self.make_occurences_Counter(element_type)
+            
+            return occurences.most_common()
         
-        number_of_words = len(words)
-        number_of_complex_words = len(complex_words)
-        number_of_sentences = len(sentences)
-        
-        return (0.4) * ( (number_of_words / number_of_sentences) 
-                         + 100 * (number_of_complex_words / number_of_words) )
+        else:
+            logging.error("No such type available cannot rank by occurrences: "
+                          + element_type)
+            logging.error("try: " + str(Text._types_of_elements))
+            sys.exit(0)
 
     def find_all_adverbs(self):
         """Finds all the adverbs in the text."""
@@ -338,10 +307,65 @@ class Text(BasicText):
                 adverbs.append(word)
         
         return adverbs
+
+
+    def _parse_phrases(self, cast_as_objects=True):
+        """Parses a Text and returns a generator containing the phrases as Phrases"""
+        phrases = self.text.split(".")
+            
+        phrases = Text._clean_input_elements(phrases, 'p')
+        
+        if cast_as_objects:
+            return [Phrase(phrase) for phrase in phrases]
+        else:
+            return phrases
     
-    def __repr__(self):
-        return self.__str__()
- 
+    def _parse_words(self, cast_as_objects=True):
+        """Parses a Text and returns a generator containing the words as Words"""
+        words = self.text.split(" ")
+        
+        words = Text._clean_input_elements(words, 'w')
+        
+        if cast_as_objects:
+            return [Word(word) for word in words]
+        else:
+            return words
+    
+    def _parse_characters(self, cast_as_objects=True):
+        """Parses a Text and returns a generator containing the characters as Characters"""
+        characters = list(self.text)
+        
+        characters = Text._clean_input_elements(characters, 'c')
+        
+        if cast_as_objects:
+            return [Character(character) for character in characters]
+        else:
+            return characters
+    
+    def _parse_elements(self, element_type):
+        """
+            Splits the text by element and returns a list
+            
+            i.e. "Bill. Is. Cool." => Words (w) => ["Bill.", "Is.", "Cool."]
+            "Bill. Is. cool." => Characters (c) => ["B", "i", "l", "l", "I", "s", ...]
+            "Bill. Is very. cool." => Phrases (p) => ["Bill", "Is very", "Cool"]
+        """
+        if element_type in Text._types_of_elements:
+            if element_type == Text._types_of_elements[0]:
+                return self._parse_words()
+            
+            elif element_type == Text._types_of_elements[2]:
+                return self._parse_phrases()
+
+            elif element_type == Text._types_of_elements[1]:
+                return self._parse_characters()
+        
+        else:
+            logging.error("No such type available cannot split: " + element_type)
+            logging.error("try: " + str(Text._types_of_elements))
+            sys.exit(0)
+
+
 def main():
     for word in ('honour', 'decode', 'decoded', 'oiseau', 'mathematical',
                  'abe','hippopotamus', 'reincarnation', 'information'):
