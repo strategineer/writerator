bfcf0b995a174898a9d5b65352271d6349415c55
Bill Tyros <tyros.bill@gmail.com>
2012-12-11 10:54:57 -0500

    add more unit tests

--- texttools_unittest.py
+++ texttools_unittest.py
@@ -31,15 +31,30 @@ class TestTextTools(unittest.TestCase):
         del self.sentence
 
     def test_init(self):
+        """"Tests init correcting .text params"""
         self.word =  Word("Bill .,")
         self.sentence = Sentence(" Bill is cool. ")
+        
         self.assertEqual(self.word.text, "Bill")
         self.assertEqual(self.sentence.text, "Bill is cool.")
         
     def test_Word_count_syllables(self):
+        
+        self.word.text = "Bill"
+        self.assertTrue(self.word.countSyllables() == 1)
+        
+        self.word.text = "massive"
+        self.assertTrue(self.word.countSyllables() == 2)
+        
+        self.word.text = 'gigantic'
+        self.assertTrue(self.word.countSyllables() == 3)
+        
+        self.word.text = 'considerable'
+        self.assertTrue(self.word.countSyllables() == 4)
+        
+    def test_Word_count(self):
         self.word.text = "Bill"
         
-        self.assertTrue(self.word.countSyllables() == 1)
         self.assertTrue(self.word.count("B") == 1)
         self.assertTrue(self.word.count("i") == 1)
         self.assertTrue(self.word.count("l") == 2)
@@ -47,10 +62,26 @@ class TestTextTools(unittest.TestCase):
         
     def test_Word_istitle(self):
         self.word.text = "Bob"
-        self.assertTrue(self.word.istitle())
+        self.assertTrue(self.word.iscapitalize())
         
-        self.word.text = "appple"
-        self.assertFalse(self.word.istitle())
+        self.word.text = "apple"
+        self.assertFalse(self.word.iscapitalize())
+    
+    def test_Word_eq(self):
+        test_word = " Bill ., "
+        self.word.text = test_word
+        word2 = Word(test_word)
+        
+        self.assertEqual(self.word, word2)
+        self.assertEqual(self.word.text, word2.text)
+    
+    def test_Sentence_eq(self):
+        test_sentence = " Bill is cool. "
+        self.sentence.text = test_sentence
+        sentence2 = Sentence(test_sentence)
+        
+        self.assertEqual(self.sentence, sentence2)
+        self.assertEqual(self.sentence.text, sentence2.text)
         
 if __name__ == '__main__':
     unittest.main()
