721fadad8394ad4da92844e11187e7ded4e1d500
Bill Christian Tyros <tyros.bill@gmail.com>
2019-06-10 22:51:07 -0400

    Refactor validation of element type parameters

--- writerator.py
+++ writerator.py
@@ -10,18 +10,6 @@ from texttools import Text
 
 ARG_TEXT = "ARG_TEXT"
 
-def expand_type(code):
-    """Expands one letter element type code to full word"""
-    if code == 'c':
-        return "characters"
-    elif code == 'w':
-        return "words"
-    elif code == 's':
-        return "sentences"
-    else:
-        logging.error("No such element type cannot expand: " + code)
-        sys.exit(1)
-
 def get_syllable_pattern(name):
     #Shakespeare Sonnet
     if name == 's':
@@ -42,6 +30,22 @@ def generate_ranked_list_output(ranked_elements):
         if count != 0:
             output_lines.append(str(count) + ": " + str(element))
     return output_lines
+
+class ElementParamType(click.ParamType):
+    name = "element"
+
+    def convert(self, value, param, ctx):
+        """Expands one letter element type code to full word"""
+        if value == 'c':
+            return "characters"
+        elif value == 'w':
+            return "words"
+        elif value == 's':
+            return "sentences"
+        else:
+            self.fail("Choices must be 'c' for characters 'w' for words 's' for sentences.", param, ctx)
+
+ELEMENT_TYPE = ElementParamType()
 
 @click.group()
 @click.argument('IN_FILE')
@@ -84,17 +88,15 @@ def info(ctx):
 
 @cli.command(help="""Count each occurrence, as defined by the -t option, of each element within the text.""")
 @click.option("-c", "--count", help="""Count the number of times a specific element appears in the text.""")
-@click.option("-t", "--type", "sequence_type", type=click.Choice(['w', 'c', 's']), default='w',
+@click.option("-t", "--type", "element_type", type=ELEMENT_TYPE, default='w',
               help="""determine the type of text elements (words, characters, sentences) to analyze""" )
 @click.pass_context
-def count(ctx, count, sequence_type):
+def count(ctx, count, element_type):
     text = ctx.obj[ARG_TEXT]
     ls = []
     if count:
-        (element_type, element_to_count) = (expand_type(sequence_type) , count)
-        ls = [str(text.count_occurences(element_to_count, element_type))]
+        ls = [str(text.count_occurences(count, element_type))]
     else:
-        element_type = expand_type(sequence_type)
         ranked_elements = text.rank_by_total_count(element_type)
         ls = generate_ranked_list_output(ranked_elements)
 
@@ -103,17 +105,16 @@ def count(ctx, count, sequence_type):
 
 @cli.command(help="""count the number of matches of a pattern (sequences of characters seperated by '~') within each one of the elements of the text""")
 @click.argument("patterns")
-@click.option("-t", "--type", "sequence_type", type=click.Choice(['w', 'c', 's']), default='w',
+@click.option("-t", "--type", "element_type", type=ELEMENT_TYPE, default='w',
               help="""determine the type of text elements (words, characters, sentences) to analyze""" )
 @click.pass_context
-def match(ctx, patterns, sequence_type):
+def match(ctx, patterns, element_type):
     text = ctx.obj[ARG_TEXT]
-    (element_type, elements_to_match) = (expand_type(sequence_type), patterns)
     match_seperator = "~"
-    if match_seperator in elements_to_match:
-        elements_to_match = elements_to_match.split(match_seperator)
+    if match_seperator in patterns:
+        patterns = patterns.split(match_seperator)
     else:
-        elements_to_match = [elements_to_match]
-    ranked_elements = text.rank_by_number_of_matches( elements_to_match , element_type )
+        patterns = [patterns]
+    ranked_elements = text.rank_by_number_of_matches(patterns , element_type)
     for l in generate_ranked_list_output(ranked_elements):
         print(l)
