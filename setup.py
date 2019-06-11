4c710c6b5df73062075ad96bc4110eadab35c307
Bill Christian Tyros <tyros.bill@gmail.com>
2019-06-10 22:36:34 -0400

    Replace argparse module with click and simplify CLI

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="poet",
+    version="1.0",
+    py_modules=['writerator'],
+    packages=find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'click'
+        ],
+    entry_points='''
+        [console_scripts]
+        poet=writerator:cli
+    ''',
+)
--- writerator.py
+++ writerator.py
@@ -3,167 +3,38 @@
 import logging
 import sys
 import os
-import shlex
-import argparse
+
+import click
 
 from texttools import Text
 
-def main():
-    parser = make_parser()
+ARG_TEXT = "ARG_TEXT"
 
-    args = parser.parse_args()
+def expand_type(code):
+    """Expands one letter element type code to full word"""
+    if code == 'c':
+        return "characters"
+    elif code == 'w':
+        return "words"
+    elif code == 's':
+        return "sentences"
+    else:
+        logging.error("No such element type cannot expand: " + code)
+        sys.exit(1)
 
-    logging_level = logging.DEBUG if args.debug else logging.ERROR
-    logging.basicConfig(stream=sys.stderr, level=logging_level)
+def get_syllable_pattern(name):
+    #Shakespeare Sonnet
+    if name == 's':
+        return get_repeat_syllable_pattern(10, 14)
+    #Haiku
+    elif name == 'h':
+        return [7,5,7]
 
-    logging.debug(str(args))
-
-    text = Text(args.infile.name)
-
-    for line in get_output(text, parser):
-        print(line)
-
-def make_parser():
-    main_parser = argparse.ArgumentParser()
-
-
-    main_parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
-                        default=sys.stdin)
-
-    main_parser.add_argument("-d", "--debug",
-                        help="displays logging debug messages.",
-                        action="store_true")
-
-    type_parent_parser = argparse.ArgumentParser(add_help=False)
-
-    type_parent_parser.add_argument("-t", "--type", choices=['w', 'c', 's'],
-                                    default='w', type=str,
-                                    help="""determine the type of text elements
-                                    (words, characters, sentences) to analyze""" )
-
-    show_parent_parser = argparse.ArgumentParser(add_help=False)
-
-    subparsers = main_parser.add_subparsers(help="commands", dest="command")
-
-    count_parser = subparsers.add_parser("count", help=
-                                           """count occurrences of elements
-                                           within the text""",
-                                           parents=[type_parent_parser, show_parent_parser])
-
-    count_parser.add_argument("-o", "--totalcount", action="store_true",
-                              help="""count each element in a text and rank them
-                                        by total count""")
-
-    count_parser.add_argument("-c", "--count", metavar="ELEMENT", type=str,
-                              help="""count the number of times ELEMENT appears
-                              in the text.""")
-
-    info_parser = subparsers.add_parser("info", help=
-                                          """get general info about the text (just the Gunning-Fog Index for now)""")
-
-    match_parser = subparsers.add_parser("match", help=
-                                           """count the number of matches of a
-                                           pattern within each one of the
-                                           elements of the text""",
-                                           parents=[type_parent_parser,
-                                                     show_parent_parser])
-
-    match_parser.add_argument("patterns", metavar="PATTERN1~PATTERN2~...",
-                              type=str,
-                              help="""patterns, separated by ~, to match within
-                              the elements.""")
-
-
-    poem_parser = subparsers.add_parser("poem", help=
-                                              """generate randomized poems""",
-                                              parents=[show_parent_parser])
-
-    poem_group = poem_parser.add_mutually_exclusive_group()
-
-    poem_group.add_argument("-l", "--syllables", nargs='+', metavar="N",
-                            type=int,
-                            help="""generate a poem by specifying the number of
-                            syllables for each line in the poem.""")
-
-    poem_group.add_argument("-p", "--preset", metavar="PRESET",
-                            choices=['h', 's'],
-                            help="""generate a poem by specifying a preset.""")
-
-    poem_group.add_argument("-c", "--shortcut", metavar="N", type=int, nargs=2,
-                            help="""generate a poem by specifying the number of
-                            syllables per line and the number of lines.""")
-
-    return main_parser
-
-def get_output(text, parser):
-    def expand_type(code):
-        """Expands one letter element type code to full word"""
-        if code == 'c':
-            return "characters"
-        elif code == 'w':
-            return "words"
-        elif code == 's':
-            return "sentences"
-
-        else:
-            logging.error("No such element type cannot expand: " + code)
-            sys.exit(1)
-
-    def get_syllable_pattern(name):
-        #Shakespeare Sonnet
-        if name == 's':
-            return get_repeat_syllable_pattern(10, 14)
-        #Haiku
-        elif name == 'h':
-            return [7,5,7]
-
-    def get_repeat_syllable_pattern(number_of_syllables, times_to_repeat):
-        repeat_pattern = []
-        for _ in range(0, times_to_repeat):
-            repeat_pattern.append(number_of_syllables)
-
-        return repeat_pattern
-
-    commands = ['poem', 'count', 'match', 'info']
-
-    args = parser.parse_args()
-
-    if args.command == commands[1]:
-        if args.count:
-            (element_type, element_to_count) = (expand_type(args.type) , args.count)
-            return [str(text.count_occurences(element_to_count, element_type))]
-
-        elif args.totalcount:
-            element_type = expand_type(args.type)
-            ranked_elements = text.rank_by_total_count(element_type)
-            return generate_ranked_list_output(ranked_elements)
-
-    elif args.command == commands[2]:
-        (element_type, elements_to_match) = (expand_type(args.type), args.patterns)
-        match_seperator = "~"
-        if match_seperator in elements_to_match:
-            elements_to_match = elements_to_match.split(match_seperator)
-        else:
-            elements_to_match = [elements_to_match]
-        ranked_elements = text.rank_by_number_of_matches( elements_to_match , element_type )
-        return generate_ranked_list_output(ranked_elements)
-
-    elif args.command == commands[0]:
-        if args.syllables:
-            syllables_pattern = args.syllables
-        elif args.preset:
-            syllables_pattern = get_syllable_pattern(args.preset)
-        elif args.shortcut:
-            syllables_pattern = get_repeat_syllable_pattern(args.shortcut[0], args.shortcut[1])
-
-        output_lines = []
-        poem = text.generate_poem(syllables_pattern)
-        for line in poem:
-            output_lines.append(line)
-        return output_lines
-
-    elif args.command == commands[3]:
-        return [str(text.calculate_Gunning_Fog_Index())]
+def get_repeat_syllable_pattern(number_of_syllables, times_to_repeat):
+    repeat_pattern = []
+    for _ in range(0, times_to_repeat):
+        repeat_pattern.append(number_of_syllables)
+    return repeat_pattern
 
 def generate_ranked_list_output(ranked_elements):
     output_lines = []
@@ -172,5 +43,77 @@ def generate_ranked_list_output(ranked_elements):
             output_lines.append(str(count) + ": " + str(element))
     return output_lines
 
-if __name__== "__main__":
-        main()
+@click.group()
+@click.argument('IN_FILE')
+@click.option('-v', '--verbose', is_flag=True, help="Enable verbose logging.")
+@click.pass_context
+def cli(ctx, in_file, verbose):
+    if ctx.obj is None:
+        ctx.obj = {}
+
+    logging_level = logging.DEBUG if verbose else logging.ERROR
+    logging.basicConfig(stream=sys.stderr, level=logging_level)
+
+    ctx.obj[ARG_TEXT] = Text(in_file)
+
+@cli.command(help='Generate randomized poems.')
+@click.option("-l", "--syllables", help="""generate a poem by specifying the number of syllables, as a string of space seperated numbers, for each line in the poem.""")
+@click.option("-p", "--preset", type=click.Choice(['h', 's']), help="""generate a poem by specifying a preset.""")
+@click.option("-c", "--shortcut", type=int, nargs=2, help="""generate a poem by specifying the number of syllables per line and the number of lines.""")
+@click.pass_context
+def poem(ctx, syllables, preset, shortcut):
+    text = ctx.obj[ARG_TEXT]
+    if syllables:
+        syllables_pattern = [int(s) for s in syllables.split(" ")]
+    elif preset:
+        syllables_pattern = get_syllable_pattern(preset)
+    elif shortcut:
+        syllables_pattern = get_repeat_syllable_pattern(shortcut[0], shortcut[1])
+    else:
+        # Default to a Haiku.
+        syllables_pattern = get_syllable_pattern('h')
+
+    for l in text.generate_poem(syllables_pattern):
+        print(l)
+
+@cli.command(help="""get general info about the text (just the Gunning-Fog Index for now)""")
+@click.pass_context
+def info(ctx):
+    text = ctx.obj[ARG_TEXT]
+    print(str(text.calculate_Gunning_Fog_Index()))
+
+@cli.command(help="""Count each occurrence, as defined by the -t option, of each element within the text.""")
+@click.option("-c", "--count", help="""Count the number of times a specific element appears in the text.""")
+@click.option("-t", "--type", "sequence_type", type=click.Choice(['w', 'c', 's']), default='w',
+              help="""determine the type of text elements (words, characters, sentences) to analyze""" )
+@click.pass_context
+def count(ctx, count, sequence_type):
+    text = ctx.obj[ARG_TEXT]
+    ls = []
+    if count:
+        (element_type, element_to_count) = (expand_type(sequence_type) , count)
+        ls = [str(text.count_occurences(element_to_count, element_type))]
+    else:
+        element_type = expand_type(sequence_type)
+        ranked_elements = text.rank_by_total_count(element_type)
+        ls = generate_ranked_list_output(ranked_elements)
+
+    for l in ls:
+        print(l)
+
+@cli.command(help="""count the number of matches of a pattern (sequences of characters seperated by '~') within each one of the elements of the text""")
+@click.argument("patterns")
+@click.option("-t", "--type", "sequence_type", type=click.Choice(['w', 'c', 's']), default='w',
+              help="""determine the type of text elements (words, characters, sentences) to analyze""" )
+@click.pass_context
+def match(ctx, patterns, sequence_type):
+    text = ctx.obj[ARG_TEXT]
+    (element_type, elements_to_match) = (expand_type(sequence_type), patterns)
+    match_seperator = "~"
+    if match_seperator in elements_to_match:
+        elements_to_match = elements_to_match.split(match_seperator)
+    else:
+        elements_to_match = [elements_to_match]
+    ranked_elements = text.rank_by_number_of_matches( elements_to_match , element_type )
+    for l in generate_ranked_list_output(ranked_elements):
+        print(l)
